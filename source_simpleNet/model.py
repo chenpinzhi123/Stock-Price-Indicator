c4a87ca739525ce4749a917a3344967734fd82f1
chenpinzhi123 <chenpinzhi1@gmail.com>
2020-02-20 14:02:30 +0800

    Refactored source codes so that simple net / LSTM can be seperated.

--- source_simpleNet/model.py
+++ source_simpleNet/model.py
@@ -36,21 +36,3 @@ class SimpleNet(nn.Module):
         out = self.fc2(out)
 
         return out
-
-# Here we define our model as a class
-class SimpleLSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.hidden_layer_size = hidden_dim
-
-        self.lstm = nn.LSTM(input_dim, hidden_dim)
-
-        self.linear = nn.Linear(hidden_dim, output_dim)
-
-        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
-                            torch.zeros(1, 1, self.hidden_layer_size))
-
-    def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
-        predictions = self.linear(lstm_out.view(len(input_seq), -1))
-        return predictions[-1]
