df98bba8cc2947123a5e3013ced2aafca388475d
chenpinzhi123 <chenpinzhi1@gmail.com>
2020-02-21 14:56:51 +0800

    Updated report.pdf

--- source_simpleNet/model.py
+++ source_simpleNet/model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#"""
 class SimpleNet(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -17,7 +16,6 @@ class SimpleNet(nn.Module):
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
         self.drop = nn.Dropout(0.1)
-        # NO sigmoid layer
         self.sig = nn.Sigmoid()
 
     def forward(self, x):
@@ -26,13 +24,9 @@ class SimpleNet(nn.Module):
            :return: A single, sigmoid activated value
         '''
         out = self.fc1(x)
-        # out = F.relu(out)  # activation on hidden layer
-
         out = self.sig(out)
         # convert to from -1 to 1
         out = 2 * out - 1
-
         out = self.drop(out)
         out = self.fc2(out)
-
         return out
